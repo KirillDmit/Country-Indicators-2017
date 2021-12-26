9ebda75508d79ae678ff344ad596a6a3cbbf36a0
KirillDmit <kirilldmitriev20015@gmail.com>
2021-12-26 17:42:57 +0500

    Update Data

--- untitled/src/Data.java
+++ untitled/src/Data.java
@@ -1,3 +1,17 @@
-public class Data {
+import java.util.ArrayList;
 
+public class Data {
+    private String country;
+    private String region;
+    private int happinessRank;
+    private double happinessScore;
+    private double whiskerHigh;
+    private double whiskerLow;
+    private double economyGdpPerCap;
+    private double family;
+    private double health;
+    private double freedom;
+    private double generosity;
+    private double trustGovernment;
+    private double dystopiaResidual;
 }
