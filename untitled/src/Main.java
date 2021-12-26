3873a54fe5af9ceb80e66a9d37ace3013db5e59e
KirillDmit <kirilldmitriev20015@gmail.com>
2021-12-26 17:43:06 +0500

    Update Main

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,7 +1,15 @@
-
+import java.sql.DriverManager;
+import java.sql.Driver;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.CallableStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args){
+
         System.out.println("OK");
     }
 }
