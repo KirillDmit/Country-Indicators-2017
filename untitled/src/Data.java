668580cc2eac17e31bcd50aec0431a05e43ba561
KirillDmit <kirilldmitriev20015@gmail.com>
2021-12-26 21:14:50 +0500

    Update Data

--- untitled/src/Data.java
+++ untitled/src/Data.java
@@ -1,3 +1,12 @@
+import java.awt.dnd.DropTarget;
+import java.awt.image.AreaAveragingScaleFilter;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Data {
@@ -14,4 +23,51 @@ public class Data {
     private double generosity;
     private double trustGovernment;
     private double dystopiaResidual;
+
+    public static void fillDB(ArrayList<String[]> rows){
+        ArrayList<Data> dataList  = new ArrayList<>();
+        for(int i = 0; i < rows.size(); i++){
+            var data = new Data();
+            var row = rows.get(i);
+            data.country = row[0];
+            data.region = row[1];
+            data.happinessRank = Integer.parseInt(row[2]);
+            data.happinessScore = Double.parseDouble(row[3]);
+            data.whiskerHigh = Double.parseDouble(row[4]);
+            data.whiskerLow = Double.parseDouble(row[5]);
+            data.economyGdpPerCap = Double.parseDouble(row[6]);
+            data.family = Double.parseDouble(row[7]);
+            data.health = Double.parseDouble(row[8]);
+            data.freedom = Double.parseDouble(row[9]);
+            data.generosity = Double.parseDouble(row[10]);
+            data.trustGovernment = Double.parseDouble(row[11]);
+            data.dystopiaResidual = Double.parseDouble(row[12]);
+
+        }
+    }
+
+    private static void addData(Data data){
+        try{
+            PreparedStatement insertStatement = Parser.connection.prepareStatement(
+                    "INSERT INTO data (country, region, happinessRank, happinessScore, whiskerHigh, whiskerLow, economyGdpPerCap, family, health, freedom, generosity, trustGovernment, dystopiaResidual) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+            );
+            insertStatement.setString(1, data.country);
+            insertStatement.setString(2, data.region);
+            insertStatement.setInt(3, data.happinessRank);
+            insertStatement.setDouble(4, data.happinessScore);
+            insertStatement.setDouble(5, data.whiskerHigh);
+            insertStatement.setDouble(6, data.whiskerLow);
+            insertStatement.setDouble(7, data.economyGdpPerCap);
+            insertStatement.setDouble(8, data.family);
+            insertStatement.setDouble(9, data.health);
+            insertStatement.setDouble(10, data.freedom);
+            insertStatement.setDouble(11, data.generosity);
+            insertStatement.setDouble(12, data.trustGovernment);
+            insertStatement.setDouble(13, data.dystopiaResidual);
+            insertStatement.execute();
+
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
 }
