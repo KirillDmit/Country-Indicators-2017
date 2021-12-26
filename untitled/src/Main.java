9390545962ce3c929b467f3584f4566fefdc9786
KirillDmit <kirilldmitriev20015@gmail.com>
2021-12-26 22:37:41 +0500

    Update Main

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,16 +1,61 @@
-import java.sql.DriverManager;
-import java.sql.Driver;
-import java.sql.Connection;
-import java.sql.Statement;
-import java.sql.PreparedStatement;
-import java.sql.CallableStatement;
-import java.sql.ResultSet;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.*;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
-    public static void main(String[] args){
+    public static Connection connection;
 
-        System.out.println("OK");
-        ArrayList<Data> data = new ArrayList<>();
+    public static void main(String[] args){
+        Task1();
+    }
+
+    private static void Task1(){
+        Map<String, Integer> res;
+        try {
+            var statement = connection.createStatement();
+            res = getTask1Data(statement);
+            var dataSet = new DefaultCategoryDataset();
+            for(var key : res.keySet())
+                dataSet.addValue(res.get(key), key, "");
+            var barChart = ChartFactory.createBarChart(
+                    "График по показателю здоровья",
+                    "Название страны",
+                    "Показатель здоровья",
+                    dataSet,
+                    PlotOrientation.VERTICAL,
+                    true, false, false
+            );
+            try {
+                ChartUtilities.saveChartAsPNG(new File("Task1.png"), barChart, 800, 600);
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    private static Map<String, Integer> getTask1Data(Statement statement){
+        Map<String, Integer> res = new HashMap<>();
+        try{
+            ResultSet rs = statement.executeQuery("SELECT COUNT(*) health FROM data GROUP BY health ORDER BY health");
+            while(rs.next()){
+                if(!rs.getString(2).equals(""))
+                    res.put(rs.getString(2), rs.getInt(1));
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        Map<String, Integer> resSorted = new HashMap<>();
+        resSorted.putAll(res);
+        return resSorted;
     }
 }
